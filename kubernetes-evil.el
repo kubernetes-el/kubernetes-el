46553b4da71ada36efacd98ce4cf188c8db1be7d
Chris Barrett <chris.d.barrett@me.com>
2017-03-25 17:44:18 +1300

    Add a package for evil bindings

---
+++ kubernetes-evil.el
@@ -0,0 +1,21 @@
+;;; kubernetes-evil.el --- Keybindings for evil-mode.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Chris Barrett
+
+;; Author: Chris Barrett <chris+emacs@walrus.cool>
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'evil)
+
+(autoload 'kubernetes-display-pods-mode-map "kubernetes")
+
+(evil-define-key 'normal kubernetes-display-pods-mode-map
+  (kbd "g") #'kubernetes-display-pods-refresh
+  (kbd "q") #'quit-window)
+
+(provide 'kubernetes-evil)
+
+;;; kubernetes-evil.el ends here
