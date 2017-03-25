0e570c285157f5ac84433b4314168059c8653879
Chris Barrett <chris.d.barrett@me.com>
2017-03-25 17:44:00 +1300

    Implement pod view buffer management

--- kubernetes.el
+++ kubernetes.el
@@ -23,6 +23,7 @@
 
 ;;; Code:
 
+(require 'subr-x)
 (autoload 'json-read-from-string "json")
 
 (defgroup kubernetes nil
@@ -34,6 +35,31 @@
   "The kubectl command used for Kubernetes commands."
   :group 'kubernetes
   :type 'string)
+
+(defface kubernetes-context-name
+  '((((class color) (background light)) :foreground "SkyBlue4")
+    (((class color) (background  dark)) :foreground "LightSkyBlue1"))
+  "Face for context names in report buffers."
+  :group 'kubernetes)
+
+(defcustom kubernetes-display-buffer-select t
+  "Whether to select Kubernetes buffers automatically."
+  :group 'kubernetes
+  :type 'boolean)
+
+(defcustom kubernetes-display-buffer-function #'kubernetes-display-buffer-fullframe
+  "The function used display a Kubernetes buffer.
+
+The function must take a single argument, which is the buffer to display."
+  :group 'kubernetes
+  :type '(radio (function-item kubernetes-display-buffer-fullframe)
+                (function-item display-buffer)
+                (function :tag "Function")))
+
+(defconst kubernetes-display-pods-buffer-name "*kubernetes pods*")
+
+
+;; Main Kubernetes query routines
 
 (defun kubernetes--kubectl (args on-success)
   "Run kubectl with ARGS.
@@ -66,6 +92,86 @@ Returns the process object for this execution of kubectl."
                (let ((result (with-current-buffer buf
                                (string-trim-right (buffer-string)))))
                  (funcall cb result)))))
+
+
+;; View management
+
+(defun kubernetes-make-set-heading-cb (marker &optional value-format-fn)
+  (unless value-format-fn
+    (setq value-format-fn #'identity))
+  (lambda (response)
+    (with-current-buffer (marker-buffer marker)
+      (save-excursion
+        (goto-char (marker-position marker))
+        (let ((inhibit-read-only t))
+          (delete-region (point) (line-end-position))
+          (insert (funcall value-format-fn response)))))))
+
+(defun kubernetes-display-buffer-fullframe (buffer)
+  (let ((display-fn
+         (lambda (buffer alist)
+           (when-let (window (or (display-buffer-reuse-window buffer alist)
+                                 (display-buffer-same-window buffer alist)
+                                 (display-buffer-pop-up-window buffer alist)
+                                 (display-buffer-use-some-window buffer alist)))
+             (delete-other-windows window)
+             window))))
+    (display-buffer buffer (list display-fn))))
+
+(defun kubernetes-display-buffer (buffer)
+  (let ((window (funcall kubernetes-display-buffer-function buffer)))
+    (when kubernetes-display-buffer-select
+      (select-frame-set-input-focus
+       (window-frame (select-window window))))))
+
+
+;;; Displaying pods
+
+;;;###autoload
+(defun kubernetes-display-pods-refresh ()
+  "Create or refresh the Kubernetes pods buffer."
+  (interactive)
+  (let ((buf (get-buffer-create kubernetes-display-pods-buffer-name)))
+    (with-current-buffer buf
+      (kubernetes-display-pods-mode)
+      (let ((inhibit-read-only t))
+        (erase-buffer)
+        (insert (format "%-10s" "Context: "))
+        (let ((marker (make-marker))
+              (context-formatter (lambda (ctx)
+                                   (if (string-empty-p ctx)
+                                       "<none>"
+                                     (propertize ctx 'face 'kubernetes-context-name)))))
+          (set-marker marker (point))
+          (kubernetes-config-current-context
+           (kubernetes-make-set-heading-cb marker context-formatter)))))
+    buf))
+
+(defvar kubernetes-display-pods-mode-map
+  (let ((keymap (make-sparse-keymap)))
+    (define-key keymap (kbd "g") #'kubernetes-display-pods-refresh)
+    (define-key keymap (kbd "q") #'quit-window)
+    keymap)
+  "Keymap for `kubernetes-display-pods-mode'.")
+
+(define-derived-mode kubernetes-display-pods-mode special-mode "Kubernetes Pods"
+  "Mode for working with Kubernetes pods.
+
+\\<kubernetes-display-pods-mode-map>\
+Type \\[kubernetes-display-pods-refresh] to refresh the buffer.
+
+\\{kubernetes-display-pods-mode-map}"
+  :group 'kubernetes
+  (read-only-mode +1))
+
+;;;###autoload
+(defun kubernetes-display-pods ()
+  "Display a list of pods in the current Kubernetes context."
+  (interactive)
+  (with-current-buffer (kubernetes-display-pods-refresh)
+    (goto-char (point-min))
+    (kubernetes-display-buffer (current-buffer))))
+
 (provide 'kubernetes)
 
 ;;; kubernetes.el ends here
