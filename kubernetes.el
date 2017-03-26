0d8c1ee066090f811f3f8291c83d1ec9885f67ad
Chris Barrett <chris.d.barrett@me.com>
2017-03-26 19:23:39 +1300

    Update package header

--- kubernetes.el
+++ kubernetes.el
@@ -6,7 +6,7 @@
 
 ;; Version: 0.0.1
 
-;; Package-Requires: ((dash "2.12.1"))
+;; Package-Requires: ((emacs "25.1") (dash "2.13.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
