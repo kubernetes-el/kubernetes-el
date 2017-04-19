ae53ea56fc9aece0b65538b567cfc992f1d40b14
Chris Barrett <chris.d.barrett@me.com>
2017-04-19 21:07:13 +1200

    Release 0.7.0

--- kubernetes-evil.el
+++ kubernetes-evil.el
@@ -4,9 +4,9 @@
 
 ;; Author: Chris Barrett <chris+emacs@walrus.cool>
 
-;; Version: 0.6.0
+;; Version: 0.7.0
 
-;; Package-Requires: ((kubernetes "0.6.0") (evil "1.2.12"))
+;; Package-Requires: ((kubernetes "0.7.0") (evil "1.2.12"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
