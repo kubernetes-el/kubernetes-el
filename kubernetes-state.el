782b222dac18aa49f4cfa30973254f5b479bd4c1
Chris Barrett <chris.d.barrett@me.com>
2017-05-02 23:19:48 +1200

    Add jobs resource type

--- kubernetes-state.el
+++ kubernetes-state.el
@@ -379,6 +379,7 @@
                                   configmaps
                                   overview
                                   deployments
+                                  jobs
                                   pods
                                   secrets
                                   services))
---
+++ test/kubernetes-jobs-test.el
@@ -0,0 +1,113 @@
+;;; kubernetes-jobs-test.el --- Test rendering of the jobs list  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require 's)
+(require 'kubernetes-jobs)
+(declare-function test-helper-json-resource "test-helper.el")
+
+
+(defconst sample-get-jobs-response (test-helper-json-resource "get-jobs-response.json"))
+
+(defun draw-jobs-section (state)
+  (kubernetes-ast-eval (kubernetes-jobs-render state)))
+
+
+;; Shows "Fetching..." when state isn't initialized yet.
+
+(defconst kubernetes-jobs-test--loading-result
+  (s-trim-left "
+
+Jobs
+  Name                                          Successful    Age
+  Fetching...
+
+"))
+
+(ert-deftest kubernetes-jobs-test--empty-state ()
+  (with-temp-buffer
+    (save-excursion (magit-insert-section (root)
+                      (draw-jobs-section nil)))
+    (should (equal kubernetes-jobs-test--loading-result
+                   (substring-no-properties (buffer-string))))
+    (forward-line 1)
+    (forward-to-indentation)
+    (should (equal 'kubernetes-progress-indicator (get-text-property (point) 'face)))))
+
+
+;; Shows "None" when there are no jobs.
+
+(defconst kubernetes-jobs-test--empty-result
+  (s-trim-left "
+
+Jobs (0)
+  None.
+
+"))
+
+(ert-deftest kubernetes-jobs-test--no-jobs ()
+  (let ((empty-state `((jobs . ((items . ,(vector)))))))
+    (with-temp-buffer
+      (save-excursion (magit-insert-section (root)
+                        (draw-jobs-section empty-state)))
+      (should (equal kubernetes-jobs-test--empty-result
+                     (substring-no-properties (buffer-string))))
+      (search-forward "None")
+      (should (equal 'magit-dimmed (get-text-property (point) 'face))))))
+
+
+;; Shows job lines when there are jobs.
+
+(defconst kubernetes-jobs-test--sample-result
+  (s-trim-left "
+
+Jobs (2)
+  Name                                          Successful    Age
+  example-job-1                                        1      10d
+    Namespace:  mm
+    Created:    2017-04-22T22:00:02Z
+    Started:    2017-04-22T22:00:03Z
+    Completed:  2017-04-23T00:00:05Z
+
+    Containers
+      Name:       example-job-1
+      Image:      docker.example.com/example-job-1:latest
+
+  example-job-2                                        1      10d
+    Namespace:  mm
+    Created:    2017-04-22T22:00:02Z
+    Started:    2017-04-22T22:00:03Z
+    Completed:  2017-04-23T00:00:05Z
+
+    Containers
+      Name:       example-job-2
+      Image:      docker.example.com/example-job-2:latest
+
+
+"))
+
+(ert-deftest kubernetes-jobs-test--sample-response ()
+  (let ((state `((jobs . ,sample-get-jobs-response)
+                 (current-time . ,(date-to-time "2017-05-03 00:00Z")))))
+    (with-temp-buffer
+      (save-excursion (magit-insert-section (root)
+                        (draw-jobs-section state)))
+      (should (equal kubernetes-jobs-test--sample-result
+                     (substring-no-properties (buffer-string)))))))
+
+(ert-deftest kubernetes-jobs-test--sample-response-text-properties ()
+  (let ((state `((jobs . ,sample-get-jobs-response))))
+    (with-temp-buffer
+      (save-excursion (magit-insert-section (root)
+                        (draw-jobs-section state)))
+      ;; Skip past header.
+      (forward-line 2)
+
+      (dolist (key '("Namespace"
+                     "Created"))
+        (save-excursion
+          (search-forward key)
+          (should (equal 'magit-header-line (get-text-property (point) 'face))))))))
+
+
+;;; kubernetes-jobs-test.el ends here
