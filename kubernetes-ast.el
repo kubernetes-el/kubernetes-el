0715ed0ec8e84d44ea8473bfd9cb2410b792b45b
Chris Barrett <chris.d.barrett@me.com>
2017-05-03 19:36:48 +1200

    Implement support for derived components

--- kubernetes-ast.el
+++ kubernetes-ast.el
@@ -9,6 +9,69 @@
 (require 'magit)
 (require 'subr-x)
 
+;; Derived component support.
+
+(defconst kubernetes-ast--components (make-hash-table :test #'eq)
+  "A mapping from the name of a component to its interpretation function.
+
+When traversing a rendering AST, any list beginning with a symbol
+is interpreted as a component reference.  That symbol is used to
+look up an interpretation function in this table.  That function is
+applied to any remaining elements of that cons.
+
+The result of a function in this hash-table should be a new
+rendering AST, or a string value to be inserted directly.")
+
+(defmacro kubernetes-ast-define-component (name arglist &rest body)
+    "Define a rendering component.
+
+NAME is the name of the component, which may thereafter be
+referenced directly in rendering ASTs.
+
+ARGLIST is the arguments that must be supplied to construct the
+component.
+
+BODY is the definition of the component."
+  (declare (indent 2))
+  (cl-assert (symbolp name))
+  (cl-assert (listp arglist))
+  (let ((fname (intern (format "kubernetes-ast--generated--%s" name)))
+        (docstring (format "Auto-generated component constructor function.
+
+Creates instances of %s components, which may be referred to as
+such in rendering ASTs." name)))
+    `(progn
+       (cl-defun ,fname ,arglist ,docstring ,@body)
+       (puthash ',name #',fname kubernetes-ast--components))))
+
+(kubernetes-ast-define-component line (inner-ast)
+  `(,inner-ast
+    (padding)))
+
+(kubernetes-ast-define-component key-value (width key value)
+  (cl-assert (numberp width) t)
+  (cl-assert (<= 0 width) t)
+  (cl-assert (stringp key) t)
+  (cl-assert (stringp value) t)
+  (let* ((fmt-string (concat "%-" (number-to-string width) "s"))
+         (str (concat (propertize (format fmt-string (concat key ": ")) 'face 'magit-header-line)
+                      value)))
+    (unless (string-blank-p (buffer-substring (line-beginning-position) (line-end-position)))
+      (newline))
+    `(copy-prop ,value (line ,str))))
+
+(kubernetes-ast-define-component nav-prop (spec &rest inner-ast)
+  `(propertize (kubernetes-nav ,spec)
+               ,inner-ast))
+
+(kubernetes-ast-define-component copy-prop (copy-str &rest inner-ast)
+  (cl-assert (stringp copy-str) t)
+  `(propertize (kubernetes-copy ,copy-str)
+               ,inner-ast))
+
+
+;; AST interpreter.
+
 (defconst kubernetes-ast--indentation-width 2)
 (defconst kubernetes-ast--space ?\ )
 
@@ -26,6 +89,7 @@ Warning: This could blow the stack if the AST gets too deep."
         ;; HACK: Temporarily increase interpreter limits for recursive calls.
         (max-lisp-eval-depth 2000)
         (max-specpdl-size    4000))
+
     (pcase render-ast
 
       ;; Core forms
@@ -34,10 +98,6 @@ Warning: This could blow the stack if the AST gets too deep."
        (insert (if (string-empty-p (buffer-substring (line-beginning-position) (point)))
                    (concat (kubernetes-ast--indentation indent-level) x)
                  x)))
-
-      (`(line ,inner-ast)
-       (kubernetes-ast-eval inner-ast indent-level)
-       (newline))
 
       (`(heading ,inner-ast)
        (unless magit-insert-section--current
@@ -50,8 +110,9 @@ Warning: This could blow the stack if the AST gets too deep."
        (eval `(magit-insert-section (,sym nil ,hide)
                 (kubernetes-ast-eval ',inner ,indent-level))))
 
-      (`(padding . ,inner)
-       (when inner (error "Eval AST: Padding takes no arguments"))
+      (`(padding . ,rest)
+       (when rest
+         (error "Padding takes no arguments"))
        (newline))
 
       (`(propertize ,spec . ,inner-ast)
@@ -76,33 +137,6 @@ Warning: This could blow the stack if the AST gets too deep."
                (insert "- "))))
          (delete-char -1)))
 
-
-      ;; Sugar forms
-
-      (`(key-value ,width ,k ,v)
-       (cl-assert (numberp width) t)
-       (cl-assert (<= 0 width) t)
-       (cl-assert (stringp k) t)
-       (cl-assert (stringp v) t)
-
-       (let* ((fmt-string (concat "%-" (number-to-string width) "s"))
-              (str (concat (propertize (format fmt-string (concat k ": ")) 'face 'magit-header-line)
-                           v)))
-         (unless (string-blank-p (buffer-substring (line-beginning-position) (line-end-position)))
-           (newline))
-         (kubernetes-ast-eval `(copy-prop ,v (line ,str)) indent-level)))
-
-      (`(nav-prop ,spec . ,inner-ast)
-       (kubernetes-ast-eval `(propertize (kubernetes-nav ,spec)
-                           ,inner-ast)
-              indent-level))
-
-      (`(copy-prop ,copy-str . ,inner-ast)
-       (cl-assert (stringp copy-str) t)
-       (kubernetes-ast-eval `(propertize (kubernetes-copy ,copy-str)
-                           ,inner-ast)
-              indent-level))
-
       (`(mark-for-delete . ,inner-ast)
        (let ((start (point)))
          (kubernetes-ast-eval inner-ast indent-level)
@@ -114,13 +148,28 @@ Warning: This could blow the stack if the AST gets too deep."
                (kubernetes-ast-put-delete-mark-on-line-at-pt (point))
                (forward-line 1))))))
 
+
+      ;; Interpret components defined in terms of core forms.
+
+      ((and `(,component . ,args)
+            (guard component)
+            (guard (symbolp component)))
+       (if-let (constructor (gethash component kubernetes-ast--components))
+           (kubernetes-ast-eval (apply constructor args) indent-level)
+         (error "Component not defined: %s" component)))
+
+
+      ;; Interpret lists of AST types.
+
       ((and actions (pred listp))
        (dolist (action actions)
          (kubernetes-ast-eval action indent-level)))
 
-
       (x
        (error "Unknown AST form: %s" x)))))
+
+
+;; Special operations.
 
 (defun kubernetes-ast-put-delete-mark-on-line-at-pt (point)
   (save-excursion
