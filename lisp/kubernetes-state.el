be525dd4679ff404bf65c9178ca5e84b250effde
Chris Barrett <chris+git@walrus.cool>
2017-05-24 17:47:44 +1200

    Add state accessors for namespace and client process

--- lisp/kubernetes-state.el
+++ lisp/kubernetes-state.el
@@ -2,8 +2,38 @@
 ;;; Commentary:
 ;;; Code:
 
-
-
 (provide 'kubernetes-state)
+
+(defun kubernetes-state-empty ()
+  (make-hash-table
+   ;; Set this to the number of top-level state attrs
+   :size 10
+   :test #'eq))
+
+(defvar kubernetes-state (kubernetes-state-empty))
+
+
+;; Accessors
+
+(defmacro kubernetes-state-defaccessors (name arglist &rest assertions)
+  (declare (indent defun))
+  (unless (symbolp name) (error "NAME must be a symbol"))
+  (unless (listp arglist) (error "ARGLIST must be a list"))
+  (unless (equal 1 (length arglist)) (error "ARGLIST must contain a single binding"))
+  `(progn
+     (defun ,(intern (format "kubernetes-state-%s" name)) ()
+       (gethash (quote ,name) kubernetes-state))
+
+     (defun ,(intern (format "kubernetes-state-set-%s" name)) ,arglist
+       ,@assertions
+       (puthash (quote ,name) ,(car arglist) kubernetes-state))))
+
+
+(kubernetes-state-defaccessors client-process (process))
+
+(kubernetes-state-defaccessors namespace (ns)
+  (unless (stringp ns)
+    (error "Namespace was not a string: %S" ns)))
+
 
 ;;; kubernetes-state.el ends here
