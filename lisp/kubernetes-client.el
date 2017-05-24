0a119f8608862a3b9ad32280c6cf114cb2e35923
Chris Barrett <chris+git@walrus.cool>
2017-05-24 20:19:28 +1200

    Improve validation of props

--- lisp/kubernetes-client.el
+++ lisp/kubernetes-client.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'let-alist)
+(require 'kubernetes-props)
 
 (defgroup kubernetes-client nil
   "Polling subprocess."
@@ -34,10 +34,10 @@ of a program in the Emacs `exec-path'."
   "Functions to inject for isolation and testing.")
 
 (defun kubernetes-client--make-line-handler-filter (props)
-  (let-alist props
+  (kubernetes-props-bind ([process-buffer process-mark handle-line] props)
     (lambda (process str)
-      (let ((buf (funcall .process-buffer process))
-            (marker (funcall .process-mark process)))
+      (let ((buf (funcall process-buffer process))
+            (marker (funcall process-mark process)))
 
         (when (buffer-live-p buf)
           (with-current-buffer buf
@@ -50,7 +50,7 @@ of a program in the Emacs `exec-path'."
                 (while (< i (length str))
                   (let ((ch (aref str i)))
                     (cond ((= ch ?\n)
-                           (funcall .handle-line (buffer-string))
+                           (funcall handle-line (buffer-string))
                            (erase-buffer))
                           (t
                            (insert ch))))
@@ -59,73 +59,77 @@ of a program in the Emacs `exec-path'."
 
               (set-marker marker (point)))))))))
 
-(defun kubernetes-client-start (props)
+(defun kubernetes-client-start (&optional props)
   "Start the kubernetes-el background process.
 
 PROPS is an alist of functions to be injected."
-  (interactive (list kubernetes-client-props))
-  (let-alist props
-    (if (funcall .get-client-process)
-        (user-error "Kubernetes client already running")
-      (funcall .message "Starting Kubernetes client")
-      (let* ((buf (generate-new-buffer " emacs-k8s"))
-             (err-buf (generate-new-buffer " emacs-k8s-err"))
-             (command
-              (list kubernetes-client-executable
-                    "-namespace" (funcall .get-namespace)
-                    "-interval" (number-to-string kubernetes-client-polling-interval)))
+       (interactive (list kubernetes-client-props))
+       (let ((props (or props kubernetes-client-props)))
+         (kubernetes-props-bind ([get-client-process set-client-process get-namespace message make-process]
+                                 props)
+           (if (funcall get-client-process)
+               (user-error "Kubernetes client already running")
+             (funcall message "Starting Kubernetes client")
+             (let* ((buf (generate-new-buffer " emacs-k8s"))
+                    (err-buf (generate-new-buffer " emacs-k8s-err"))
+                    (command
+                     (list kubernetes-client-executable
+                           "-namespace" (funcall get-namespace)
+                           "-interval" (number-to-string kubernetes-client-polling-interval)))
 
-             (process
-              (funcall .make-process
-                       :name "emacs-k8s"
-                       :buffer buf
-                       :stderr err-buf
-                       :command command
-                       :noquery t
-                       :filter (kubernetes-client--make-line-handler-filter props)
-                       :sentinel
-                       (lambda (process _status)
-                         (let ((kill-buffer-query-functions nil)
-                               (exit-code (process-exit-status process)))
-                           (cond
-                            ;; Shut down gracefully.
-                            ((= 0 exit-code))
-                            ;; Killed by Emacs.
-                            ((= 9 exit-code))
-                            ;; Unknown failure.
-                            (t
-                             (with-current-buffer err-buf
-                               (funcall .message "Kubernetes client exited unexpectedly: %s" (buffer-string)))))
-                           (kill-buffer buf)
-                           (ignore-errors (kill-buffer err-buf)))))))
+                    (process
+                     (funcall make-process
+                              :name "emacs-k8s"
+                              :buffer buf
+                              :stderr err-buf
+                              :command command
+                              :noquery t
+                              :filter (kubernetes-client--make-line-handler-filter props)
+                              :sentinel
+                              (lambda (process _status)
+                                (let ((kill-buffer-query-functions nil)
+                                      (exit-code (process-exit-status process)))
+                                  (cond
+                                   ;; Shut down gracefully.
+                                   ((= 0 exit-code))
+                                   ;; Killed by Emacs.
+                                   ((= 9 exit-code))
+                                   ;; Unknown failure.
+                                   (t
+                                    (with-current-buffer err-buf
+                                      (funcall message "Kubernetes client exited unexpectedly: %s" (buffer-string)))))
+                                  (kill-buffer buf)
+                                  (ignore-errors (kill-buffer err-buf)))))))
 
-        (funcall .set-client-process process)
+               (funcall set-client-process process)
 
-        ;; Clean up process if the process buffer is killed.
-        (with-current-buffer buf
-          (add-hook 'kill-buffer-hook #'kubernetes-client-stop nil t))
+               ;; Clean up process if the process buffer is killed.
+               (with-current-buffer buf
+                 (add-hook 'kill-buffer-hook #'kubernetes-client-stop nil t))
 
-        process))))
+               process)))))
 
 (defun kubernetes-client-stop (props)
   "Stop the kubernetes-el background process.
 
 PROPS is an alist of functions to be injected."
        (interactive (list kubernetes-client-props))
-       (let-alist props
-         (let ((process (funcall .get-client-process)))
-           (cond
-            (process
-             (set-process-query-on-exit-flag process nil)
-             (let ((kill-buffer-query-functions nil)
-                   (buf (process-buffer process)))
-               (ignore-errors (kill-process process))
-               (ignore-errors (delete-process process))
-               (ignore-errors (kill-buffer buf)))
-             (funcall .set-client-process nil)
-             (funcall .message "Kubernetes client stopped"))
-            (t
-             (user-error "Kubernetes client not running"))))))
+       (let ((props (or props kubernetes-client-props)))
+         (kubernetes-props-bind ([get-client-process set-client-process message]
+                                 props)
+           (let ((process (funcall get-client-process)))
+             (cond
+              (process
+               (set-process-query-on-exit-flag process nil)
+               (let ((kill-buffer-query-functions nil)
+                     (buf (process-buffer process)))
+                 (ignore-errors (kill-process process))
+                 (ignore-errors (delete-process process))
+                 (ignore-errors (kill-buffer buf)))
+               (funcall set-client-process nil)
+               (funcall message "Kubernetes client stopped"))
+              (t
+               (user-error "Kubernetes client not running")))))))
 
 (provide 'kubernetes-client)
 
---
+++ lisp/kubernetes-props.el
@@ -0,0 +1,45 @@
+;;; kubernetes-props.el --- Utilities for working with props. -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(require 'dash)
+(require 'subr-x)
+
+(defmacro kubernetes-props-bind (varlist &rest body)
+  "Bind a number of keys in an alist, asserting the keys are defined.
+
+VARLIST is a list of 2 elements:
+
+1. A vector of KEYS to bind, and
+2. The target ALIST to scrutinise.
+
+\(fn ([KEYS...] ALIST) &rest BODY)"
+  (declare (indent 1) (debug t))
+  (unless (listp varlist)
+    (error "Binding form must be a list"))
+  (unless (equal 2 (length varlist))
+    (error "Must provide a list of keys to bind and an alist"))
+
+  (-let [(keys alist) varlist]
+    (unless (vectorp keys)
+      (error "First item in binding list must be a vector of symbols"))
+
+    (let* ((props (make-symbol "props"))
+           (keys (append keys nil))
+           (missing-keys (make-symbol "missing-keys"))
+           (alist-binder (cons '&alist (--mapcat `((quote ,it) ,it) keys))))
+
+      (-when-let (invalid (--map (format "%s" it) (-reject #'symbolp keys)))
+        (error "Keys must be symbols. Rejected: [%s]" invalid))
+
+      `(let ((,props ,alist))
+         (unless (listp ,props)
+           (error "Attempted to bind props on a non-list object: %s" ,props))
+         (-when-let (,missing-keys (-map #'symbol-name (-difference (quote ,keys) (-map #'car ,props))))
+           (error "Props were missing the following keys: [%s]" (string-join ,missing-keys " ")))
+         (-let [,alist-binder ,props]
+           ,@body)))))
+
+(provide 'kubernetes-props)
+
+;;; kubernetes-props.el ends here
