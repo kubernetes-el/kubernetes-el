a17247fe7f134ccda204191f170d3a959314c398
Chris Barrett <chris+git@walrus.cool>
2017-05-30 22:38:41 +1200

    Show more info in pods list

--- lisp/kubernetes-pods-list.el
+++ lisp/kubernetes-pods-list.el
@@ -6,17 +6,77 @@
 
 (require 'kubernetes-ast)
 
-(kubernetes-ast-define-component pod-list-entry (pod)
-  (-let [(&alist 'metadata (&alist 'name name)) pod]
+(kubernetes-ast-define-component pod-container (container-spec container-status)
+  (-let* (((&alist 'name name 'image image) container-spec)
+          ((&alist 'state (state &as &alist
+                                 'running running
+                                 'terminated (terminated &as &alist 'exitCode code)
+                                 'waiting waiting)
+                   'restartCount restart-count)
+           container-status)
+          (started-at
+           (car (--map (alist-get 'startedAt it)
+                       (list running terminated waiting))))
+          (state
+           (cond
+            ((null container-status)
+             (propertize "Pending" 'face 'font-lock-comment-face))
+            (running
+             (propertize "Running" 'face 'success))
+            ((and terminated (zerop code))
+             (propertize (alist-get 'reason terminated) 'face 'success))
+            (terminated
+             (propertize (alist-get 'reason terminated) 'face 'error))
+            (waiting
+             (propertize (alist-get 'reason waiting) 'face 'warning))
+            (t
+             (message "Unknown state: %s" (prin1-to-string state))
+             (propertize "Warn" 'face 'warning)))))
+
+    `(section (pod-container t)
+              (heading ,(concat state " " name))
+              (key-value 12 "Image" ,image)
+              (key-value 12 "Restarts" ,(when restart-count (number-to-string restart-count)))
+              (key-value 12 "Started At" ,started-at))))
+
+(kubernetes-ast-define-component pod-container-list (containers container-statuses)
+  (when-let ((entries
+              (--map (-let* (((&alist 'name name) it)
+                             (status (-find (-lambda ((&alist 'name status-name))
+                                              (equal name status-name))
+                                            (append container-statuses nil))))
+                       `(pod-container ,it ,status))
+                     (append containers nil))))
+    `(section (containers)
+              (heading "Containers")
+              (list ,@entries))))
+
+(kubernetes-ast-define-component pod (pod)
+  (-let* (((&alist 'metadata (&alist 'name name
+                                     'namespace namespace
+                                     'labels labels)
+                   'spec (&alist 'containers containers)
+                   'status (&alist 'containerStatuses container-statuses))
+           pod)
+          ((_ . label) (--first (equal "name" (car it)) labels))
+          ((_ . job-name) (--first (equal "job-name" (car it)) labels)))
+
     `(section (pod-entry t)
-              (heading ,name))))
+              (heading ,name)
+              (indent
+               (section (label) (key-value 12 "Label" ,label))
+               (section (job-name) (key-value 12 "Job Name" ,job-name))
+               (section (namespace) (key-value 12 "Namespace" ,namespace))
+               (padding)
+               (pod-container-list ,containers ,container-statuses))
+              (padding))))
 
 (kubernetes-ast-define-component pods-list (state)
   (-let [(&hash 'pods pods) state]
     `(section (pods-list nil)
               (heading "Pods")
               (indent
-               ,(--map `(pod-list-entry ,(gethash it pods))
+               ,(--map `(pod ,(gethash it pods))
                        (kubernetes-pods-list--sorted-keys pods)))
               (padding))))
 
