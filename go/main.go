92a2aae0325ab24efff888c7322a8670dd863b58
kalmanb <kalman@movio.co>
2017-05-30 19:53:08 +1200

    Add buffered output, possibly faster

--- go/main.go
+++ go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"os"
 )
@@ -10,12 +11,17 @@ func main() {
 	var interval = flag.Int("interval", 10, "Refresh interveal in seconds, defaults to 10 secounds")
 	flag.Parse()
 
+	out := bufio.NewWriter(os.Stdout)
 	writer := make(chan []byte)
 	go func() {
 		for {
 			b := <-writer
 			b = append(b, byte('\n'))
-			_, err := os.Stdout.Write(b)
+			_, err := out.Write(b)
+			if err != nil {
+				panic("Could not write to stdout")
+			}
+			err = out.Flush()
 			if err != nil {
 				panic("Could not write to stdout")
 			}
