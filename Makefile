kubernetes-el/kubernetes-el

commit 4cbe7b1208f198abf2bcf48ce7c7b3e9432414e3

Added functionality to exec into a pod using `vterm` (#169)
Added a new action `kubernetes-exec-using-vterm` to exec into a
container using the great VTerm module.

kubernetes-commands.el
@@ -333,6 +333,44 @@ Should be invoked via command `kubernetes-exec-popup'."
     (select-window (display-buffer buf))))
 
 
+;;;###autoload
+(defun kubernetes-exec-using-vterm (pod-name args exec-command state)
+  "Open a vterm terminal for exec into a pod.
+
+POD-NAME is the name of the pod to exec into.
+
+ARGS are additional args to pass to kubectl.
+
+EXEC-COMMAND is the command to run in the container.
+
+STATE is the current application state.
+
+Should be invoked via command `kubernetes-exec-popup'."
+  (unless (featurep 'vterm)
+    (error "This action requires the vterm package."))
+
+  (interactive (let* ((state (kubernetes-state))
+                      (pod-name (or (kubernetes-utils-maybe-pod-name-at-point) (kubernetes-utils-read-pod-name state)))
+                      (command
+                       (let ((cmd (string-trim (read-string (format "Command (default: %s): " kubernetes-default-exec-command)
+                                                            nil 'kubernetes-exec-history))))
+                         (if (string-empty-p cmd) kubernetes-default-exec-command cmd))))
+                 (list pod-name (kubernetes-exec-arguments) command state)))
+
+  (let* ((command-args (append (list "exec") (kubernetes-kubectl--flags-from-state (kubernetes-state))
+                               args
+                               (when-let (ns (kubernetes-state-current-namespace state))
+                                 (list (format "--namespace=%s" ns)))
+                               (list pod-name "--" exec-command)))
+
+         (interactive-tty (member "-t" args))
+         (buf
+          (if interactive-tty
+              (kubernetes-utils-vterm-start kubernetes-exec-vterm-buffer-name
+                                            kubernetes-kubectl-executable
+                                            command-args)
+            (error "VTerm is supported only for interactive session"))))))
+
 ;; View management
 
 (defun kubernetes-commands-display-buffer-fullframe (buffer)

kubernetes-utils.el
@@ -5,6 +5,7 @@
 (require 'dash)
 (require 'subr-x)
 (require 'term)
+(require 'vterm)
 
 (require 'kubernetes-ast)
 (require 'kubernetes-kubectl)
@@ -131,6 +132,19 @@ buffer is killed."
                   nil t)))
 
     buf))
+
+(defun kubernetes-utils-vterm-start (bufname command args)
+  ;; Kill existing process.
+  (when-let ((existing (get-buffer bufname)))
+    (let ((proc (get-buffer-process existing)))
+      (if proc
+          (kubernetes-process-kill-quietly proc)
+        (kill-buffer bufname))))
+
+  (let* ((vterm-buffer-name bufname)
+         (command-str (format "%s %s" command (string-join args " ")))
+         (vterm-shell command-str))
+    (vterm-other-window)))
 
 (defun kubernetes-utils-process-buffer-start (bufname setup-fn command args &optional process-filter)
   (let ((buf (get-buffer-create bufname)))

kubernetes-popups.el
@@ -73,7 +73,8 @@
   '("Options for customizing exec behaviour"
     (?c "Container to exec within" "--container=" kubernetes-utils-read-container-name))
   :actions
-  '((?e "Exec" kubernetes-exec-into))
+  '((?e "Exec" kubernetes-exec-into)
+    (?v "Exec into container using vterm" kubernetes-exec-using-vterm))
   :default-action 'kubernetes-exec-into)
 
 (magit-define-popup kubernetes-describe-popup

kubernetes-vars.el
@@ -208,6 +208,8 @@ form \"--flag=value\" or \"-flag\"."
 
 (defconst kubernetes-exec-buffer-name "*kubernetes exec*")
 
+(defconst kubernetes-exec-vterm-buffer-name "*kubernetes exec vterm*")
+
 (defconst kubernetes-label-query-buffer-name "*kubernetes-label-query*")
 
 

commit 02d49b9e7783186b622f846460f7ab1486dcc83b

Fixed bugs introduced in #169
- Send compile output to stdout also.
- Do not consider warnings as errors for the time being.

Makefile
@@ -27,7 +27,7 @@ TAR     := dist/kubernetes-$(VERSION).tar
 build : compile $(DEPS_PNG)
 
 compile: $(SRCS) $(CASKDIR)
-	! (${CASK} eval "(let ((byte-compile-error-on-warn t)) (cask-cli/build))" 2>&1 | egrep -a "(Warning|Error):") ; (ret=$$? ; ${CASK} clean-elc && exit $$ret)
+	! (${CASK} eval "(cask-cli/build)" | tee 2>&1 | egrep -a "(Error):") ; (ret=$$? ; ${CASK} clean-elc && exit $$ret)
 
 
 dist : $(TAR)

kubernetes-commands.el
@@ -346,9 +346,6 @@ EXEC-COMMAND is the command to run in the container.
 STATE is the current application state.
 
 Should be invoked via command `kubernetes-exec-popup'."
-  (unless (featurep 'vterm)
-    (error "This action requires the vterm package."))
-
   (interactive (let* ((state (kubernetes-state))
                       (pod-name (or (kubernetes-utils-maybe-pod-name-at-point) (kubernetes-utils-read-pod-name state)))
                       (command
@@ -357,19 +354,21 @@ Should be invoked via command `kubernetes-exec-popup'."
                          (if (string-empty-p cmd) kubernetes-default-exec-command cmd))))
                  (list pod-name (kubernetes-exec-arguments) command state)))
 
+  (unless (require 'vterm nil 'noerror)
+    (error "This action requires the vterm package."))
+
   (let* ((command-args (append (list "exec") (kubernetes-kubectl--flags-from-state (kubernetes-state))
                                args
                                (when-let (ns (kubernetes-state-current-namespace state))
                                  (list (format "--namespace=%s" ns)))
                                (list pod-name "--" exec-command)))
 
-         (interactive-tty (member "-t" args))
-         (buf
-          (if interactive-tty
-              (kubernetes-utils-vterm-start kubernetes-exec-vterm-buffer-name
-                                            kubernetes-kubectl-executable
-                                            command-args)
-            (error "VTerm is supported only for interactive session"))))))
+         (interactive-tty (member "-t" args)))
+    (if interactive-tty
+        (kubernetes-utils-vterm-start kubernetes-exec-vterm-buffer-name
+                                      kubernetes-kubectl-executable
+                                      command-args)
+      (error "VTerm is supported only for interactive session"))))
 
 ;; View management
 

kubernetes-utils.el
@@ -5,7 +5,6 @@
 (require 'dash)
 (require 'subr-x)
 (require 'term)
-(require 'vterm)
 
 (require 'kubernetes-ast)
 (require 'kubernetes-kubectl)

test/test-helper.el
@@ -29,7 +29,6 @@
 
 (require 'kubernetes (f-join project-root "kubernetes.el"))
 
-
 ;; Resources
 
 (defun test-helper-string-resource (name)