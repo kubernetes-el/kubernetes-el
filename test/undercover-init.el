a1610c8d2a85d832ca6a6df773a477379410eeb7
Jonathan Jin <me@jonathanj.in>
2021-11-28 12:53:09 -0500

    Cleanup

--- test/undercover-init.el
+++
@@ -1,6 +0,0 @@
-(when (require 'undercover nil t)
-  (with-no-warnings
-    (undercover "*.el"
-                (:exclude "kubernetes-evil.el")
-                (:report-format 'lcov)
-                (:send-report nil))))
