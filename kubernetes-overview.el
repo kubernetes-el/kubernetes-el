5cc135507a2dd4426850c312b0c83a5ca52fc4b8
florisie <floris.nicolai@hotmail.com>
2022-06-29 11:36:20 +0200

    copy default column settings to state at application startup

--- kubernetes-overview.el
+++ kubernetes-overview.el
@@ -420,6 +420,7 @@ Type \\[kubernetes-refresh] to refresh the buffer.
   (interactive)
   (unless (executable-find kubernetes-kubectl-executable)
     (error "Executable for `kubectl' not found on PATH; make sure `kubernetes-kubectl-executable' is valid"))
+  (kubernetes-overview--load-default-columns)
   (let ((dir default-directory)
         (buf (kubernetes-overview--initialize-buffer)))
     (when kubernetes-default-overview-namespace
@@ -429,6 +430,42 @@ Type \\[kubernetes-refresh] to refresh the buffer.
     (with-current-buffer buf
       (cd (kubernetes-utils-up-to-existing-dir dir)))
     (message (substitute-command-keys "\\<kubernetes-overview-mode-map>Type \\[kubernetes-overview-set-sections] to switch between resources, and \\[kubernetes-dispatch] for usage."))))
+
+(defun kubernetes-overview--load-default-columns ()
+  "Copy default-column settings for all resource-types to state."
+  (let ((resources '(configmaps-columns
+                     deployments-columns
+                     ingress-columns
+                     jobs-columns
+                     nodes-columns
+                     persistentvolumeclaims-columns
+                     pods-columns
+                     secrets-columns
+                     services-columns
+                     statefulsets-columns)))
+    (dolist (key resources)
+      (when (not (alist-get key kubernetes-state--current-state))
+        (pcase key
+          ('configmaps-columns
+           (kubernetes-state-update :update-configmaps-columns kubernetes-configmaps--default-columns))
+          ('deployments-columns
+           (kubernetes-state-update :update-deployments-columns kubernetes-deployments--default-columns))
+          ('ingress-columns
+           (kubernetes-state-update :update-ingress-columns kubernetes-ingress--default-columns))
+          ('jobs-columns
+           (kubernetes-state-update :update-jobs-columns kubernetes-jobs--default-columns))
+          ('nodes-columns
+           (kubernetes-state-update :update-nodes-columns kubernetes-nodes--default-columns))
+          ('persistentvolumeclaims-columns
+           (kubernetes-state-update :update-persistentvolumeclaims-columns kubernetes-persistentvolumeclaims--default-columns))
+          ('pods-columns
+           (kubernetes-state-update :update-pods-columns kubernetes-pods--default-columns))
+          ('secrets-columns
+           (kubernetes-state-update :update-secrets-columns kubernetes-secrets--default-columns))
+          ('services-columns
+           (kubernetes-state-update :update-services-columns kubernetes-services--default-columns))
+          ('statefulsets-columns
+           (kubernetes-state-update :update-statefulsets-columns kubernetes-statefulsets--default-columns)))))))
 
 
 (provide 'kubernetes-overview)
